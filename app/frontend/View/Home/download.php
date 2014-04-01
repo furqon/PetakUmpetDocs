db85c26d56a5c653589a65edb734dec0e58d6abd
Arief M Utama <arief.utama@gmail.com>
2014-04-01 16:01:14 +0700

    Update home pages
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- app/frontend/View/Home/download.php
+++ app/frontend/View/Home/download.php
@@ -2,8 +2,11 @@
   <div class="col-md-6">
     <h2>Download</h2>
     <p>
-    PetakUmpet can be downloaded directly from github, at:<br> 
+    PetakUmpet can be cloned or downloaded directly from github, at:<br> 
     <strong><a href="https://github.com/ariefmulya/PetakUmpet">https://github.com/ariefmulya/PetakUmpet</a></strong>
+    </p>
+    <p>Direct download link to latest code here:<br>
+    <strong><a href="https://github.com/ariefmulya/PetakUmpet/archive/master.zip">https://github.com/ariefmulya/PetakUmpet/archive/master.zip</a></strong>
     </p>
     <p><h3>Open Source</h3>
     Petak Umpet is Open-Source software, we welcome everyone to contribute to its development. 
