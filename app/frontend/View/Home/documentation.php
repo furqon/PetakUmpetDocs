954b7e49a5d82a8d6ca7d964d9e514d4832dc449
Arief M Utama <arief.utama@gmail.com>
2014-04-07 10:42:39 +0700

    Added PetakUmpet API doc

--- app/frontend/View/Home/documentation.php
+++ app/frontend/View/Home/documentation.php
@@ -7,5 +7,8 @@
     <p>
       In the meantime you can download an introductory documentation <strong><a href="http://petak-umpet.com/uploads/PetakUmpet-phpWebFramework-v1.3.pdf">here</a></strong>. 
     </p>
+    <p>
+      API documentation is accessible from here: <strong><a href="/PetakUmpetAPI/">PetakUmpet API documentation</a></strong>
+    </p>
   </div>
 </div>
