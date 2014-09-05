404f1afbe8039d631a7485960a6e006f3db7e166
Arief M Utama <arief.utama@gmail.com>
2014-09-05 14:32:29 +0700

    Login page minor update
    Signed-off-by: Arief M Utama <arief.utama@gmail.com>

--- app/frontend/View/Login/index.php
+++ app/frontend/View/Login/index.php
@@ -5,9 +5,11 @@
   <?php $T->snippet('flash-message'); ?>
   <?php echo $form; ?>
 </div>
+<!-- this one is necessary to prevent funny logout situations
+   where session expired but we are still in middle of a page -->
 <script type="text/javascript">
 $(document).ready(function () {
-  if (window.top.location != '<?php $url = $request->getAppUrl("Login/index"); echo $url; ?>') {
+  if (window.top.location != '<?php $url = $request->getBaseUrl() . $config->getRoutingLinkFromPage("Login/index"); echo $url; ?>') {
     window.top.location = '<?php echo $url ?>' ;
   }
 });
